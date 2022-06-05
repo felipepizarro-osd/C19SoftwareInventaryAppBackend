e4a2d5df82c99b1e87d61279695fdaffccc0a702
MarceloAGD <marcelo.guerra@alumnos.ucn.cl>
2022-06-04 21:56:33 -0400

    Update cors police

--- src/app.js
+++ src/app.js
@@ -13,9 +13,11 @@ app.use(morgan('dev'))
 //recibe json
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+  
 app.use(express.json());
 //reciba formularios html
 app.use(express.urlencoded({extended:false}));
