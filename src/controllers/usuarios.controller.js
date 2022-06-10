d0343c7c0278c6432ea81b36c07d5483c3ed80e2
Fabri211 <52009330+Fabri211@users.noreply.github.com>
2022-06-10 00:19:28 -0400

    Cambios backend usuarios

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,22 +1,22 @@
 import {conn,sql,queries} from '../database'
 export const crearUsuario = async(req,res) => {
-    const {Rut, Nombre, Contrasena} = req.body
-    if(Rut == null || Nombre == null || Contrasena == null){
+    const {rut, Nombre, contrasena} = req.body
+    if(rut == null || Nombre == null || contrasena == null){
         return res.status(400).json({msg:'Bad Request. Please Fill all Fields'})
     };
     try{
         const pool = await conn()
         await pool.request()
-        .input('Rut',sql.VarChar,Rut)
+        .input('Rut',sql.VarChar,rut)
         .input('Nombre',sql.VarChar,Nombre)
-        .input('Contrasena',sql.VarChar,Contrasena).query(queries.createUsuario)
-        await res.json({Rut, Nombre, Contrasena})
+        .input('Contrasena',sql.VarChar,contrasena).query(queries.createUsuario)
+        await res.json({rut, Nombre, contrasena})
     }catch (error){
         await res.status(500)
         await res.send(error.message)
     }
 }
-export const getUsuario = async (req,res)=> {
+export const getUsuarios = async (req,res)=> {
     try {
         const pool = await conn();
         const result = await pool.request().query(queries.getAllUsuarios);
@@ -27,11 +27,11 @@ export const getUsuario = async (req,res)=> {
         res.send(error.message);
     }
 };
-export const getUsuariobyRUT  = async (req,res)=>{
-    const {rut} = req.params;
+export const getUsuario  = async (req,res)=>{
+    const {rut,contrasena} = req.params;
     //console.log(sku);
     const pool = await conn();
-    const result = await pool.request().input('rut',sql.VarChar,rut).query(queries.getUsuariosByRut)
+    const result = await pool.request().input('rut',sql.VarChar,rut).input('contrasena',sql.VarChar,contrasena).query(queries.getUsuario)
     //console.log(result);
-    res.send(result.recordset)
+    await res.send(result.recordset[0])
 }
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,8 +1,8 @@
 import {Router} from "express";
-import { crearUsuario, getUsuario, getUsuariobyRUT } from "../controllers/usuarios.controller.js";
+import { crearUsuario, getUsuarios, getUsuario } from "../controllers/usuarios.controller.js";
 
 const router = Router();
 router.post('/usuarios', crearUsuario)
-router.get('/usuarios', getUsuario)
-router.get('/usuarios/:rut', getUsuariobyRUT)
+router.get('/usuarios', getUsuarios)
+router.get('/usuarios/:rut', getUsuario)
 export default router;
