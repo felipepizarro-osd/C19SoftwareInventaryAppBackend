8b2b10637a5b9d632bdc5f9cdae9438365035500
Felipe Pizarro <66143232+felipepizarro-osd@users.noreply.github.com>
2022-06-27 09:15:40 -0400

    Change product controller
    1- falta crear la bodega si es que no se puede

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,6 +10,18 @@ export const getProducts = async (req,res)=> {
         res.send(error.message);
     }
 };
+export const getProductsEstanteria = async (req,res)=> {
+    try {
+        const pool = await conn();
+        const result = await pool.request().query(queries.getAllProductEstanteria);
+        //console.log(result);
+        await res.json(result.recordset)
+    } catch (error) {
+        await res.status(500);
+        await res.send(error.message);
+    }
+
+};
 export const createNewProduct = async (req,res)=>{
     const {sku,Nombre,Nombre_Servicio,Part_Number,Stock_min,Unidad} = req.body
     let {Stock} = req.body;
@@ -34,6 +46,38 @@ export const createNewProduct = async (req,res)=>{
     } catch (error) {
         res.status(500);
         res.send(error.message);        
+    }
+}
+export const createNewProductBodega = async (req,res)=>{
+    const {sku,Nombre,Nombre_Servicio,Part_Number,Stock_min,Unidad,Bodega,Modulo,Posicion} = req.body
+    let {Stock} = req.body;
+    
+    if (Stock == null) Stock = 0; 
+
+    if (Nombre == null || sku == null || Nombre_Servicio == null || Part_Number == null || Stock_min == null || Unidad == null){
+        return res.status(400).json({msg:'Bad Request. Please Fill all Fields'})
+    };
+    console.log(req.body)
+    try {
+        const pool = await conn();
+        await pool.request().input('sku',sql.VarChar,sku)
+        .input('Nombre', sql.VarChar,Nombre)
+        .input('Nombre_servicio',sql.VarChar,Nombre_Servicio)
+        .input('Part_Number',sql.VarChar,Part_Number)
+        .input('Stock',sql.Int,Stock)
+        .input('Stock_min',sql.Int,Stock_min)
+        .input('Unidad',sql.VarChar,Unidad).query(queries.createNewProduct);
+        await pool.request().input('Bodega',sql.VarChar,Bodega)
+        .input('Modulo',sql.VarChar,Modulo)
+        .input('Posicion',sql.VarChar,Posicion)
+        .input('Sku_Producto',sql.VarChar,sku)
+        .input('Num_Prod_Guardados',sql.Int,Stock).query(queries.createEstanteria)
+
+        console.log(sku,Nombre,Nombre_Servicio,Part_Number,Stock,Stock_min,Unidad);
+        await res.json({sku,Nombre,Nombre_Servicio,Part_Number,Stock,Stock_min,Unidad,Bodega,Modulo,Posicion});
+    } catch (error) {
+        await res.status(500);
+        await res.send(error.message);        
     }
 }
 export const getProductById = async (req,res)=>{
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,11 @@
 import {Router} from "express";
-import { createNewProduct, getProducts ,getProductById, deleteById, updateProducts} from "../controllers/product.controller"; 
-
+//import { createNewProduct, getProducts ,getProductById, deleteById, updateProducts} from "../controllers/product.controller"; 
+import { createNewProductBodega, getProducts ,getProductById, deleteById, updateProducts, getProductsEstanteria} from "../controllers/product.controller"; 
 const router = Router();
 
-router.get('/products',getProducts)
-
-router.post('/products',createNewProduct)
+//router.get('/products',getProducts)
+router.get('/products',getProductsEstanteria)
+router.post('/products',createNewProductBodega)
 
 router.delete('/products/:sku',deleteById)
 
