e658205b3b8e12e7533084b29d96be1441343a9f
Diego Barros <diego.barros@alumnos.ucn.cl>
2022-07-21 01:17:41 -0400

    console log eliminated

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -37,31 +37,27 @@ export const getUsuariobyRUT  = async (req,res)=>{
 }
 export const getUsuariobyRutPassword  = async (request,response)=>{
     const {rut,contrasena} = request.body;
-	console.log(request.body);
-	// Ensure the input fields exists and are not empty
-	console.log("paso por aqui")
-	if (rut && contrasena) {
-		const pool = await conn();
-		// Execute SQL query that'll select the account from the database based on the specified rut and password
-		const result = await pool.request().input('rut',sql.VarChar,rut).input('contrasena',sql.VarChar,contrasena).query(queries.getUsuariosByRutContrasena)
-		console.log(result)
-			// If there is an issue with the query, output the error
-			// If the account exists
-			if (result.recordset.length > 0) {
-				// Authenticate the user
-				//request.session.loggedin = true;
-				//request.session.rut = rut;
-				// Redirect to home page
-				//response.redirect('http://localhost:3000/');
-				response.send(result.recordset[0].Nombre);
-				//console.log('entro')
-			} else {
-				response.send('Incorrect rut and/or Password!');
-			}			
-			response.end();
-		
-	} else {
-		response.send('Please enter rut and Password!');
-		response.end();
-	}
+    if(rut == null || contrasena == null){
+        return response.status(400).json({msg:'Bad Request. Please Fill all Fields'})
+    };
+    try{
+        if (rut && contrasena) {
+            const pool = await conn();
+            const result = await pool.request().input('rut',sql.VarChar,rut).input('contrasena',sql.VarChar,contrasena).query(queries.getUsuariosByRutContrasena)
+                if (result.recordset.length > 0) {
+                    response.send(result.recordset[0].Nombre);
+
+                } else {
+                    response.send('Incorrect rut and/or Password!');
+                }			
+                response.end();
+            
+        } else {
+            response.send('Please enter rut and Password!');
+            response.end();
+        }
+    }catch (error){
+        await res.status(500)
+        await res.send(error.message)
+    }
 }
