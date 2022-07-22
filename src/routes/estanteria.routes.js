f922d48f889a09a41827b6befddc855654ed727f
Casze <felipe.cortes03@alumnos.ucn.cl>
2022-07-22 04:48:27 -0400

    Backend funcionando 4.48

--- src/routes/estanteria.routes.js
+++ src/routes/estanteria.routes.js
@@ -1,9 +1,17 @@
 import {Router} from "express";
-import { crearEstanteria,  getModuloYPosicionByUbicacion } from "../controllers/estanteria.controller.js";
+import { crearEstanteria,  
+    getModuloYPosicionByUbicacion,
+    getEstanteria,
+    crearEstanteriaX,
+    deleteByCombinacion
+} from "../controllers/estanteria.controller.js";
 
 const router = Router();
-router.post('/estanterias',crearEstanteria)
-router.get('/estanterias/:bodega', getModuloYPosicionByUbicacion)
 
+router.get('/estanterias', getEstanteria)
+router.get('/estanterias/:bodega', getModuloYPosicionByUbicacion)
+router.post('/estanterias',crearEstanteriaX)
+router.delete('/estanterias',deleteByCombinacion)
+//router.post('/estanterias/:detalle',crearEstanteriaX)
 
 export default router;
